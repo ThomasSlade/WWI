e481d7c21d349511f8dff4db465c6487a153a456
ThomasSlade <1303035@live.abertay.ac.uk>
2020-03-16 18:43:58 +0000

    Created repository.

---
+++ common/defines/wwi_graphics.lua
@@ -0,0 +1,8 @@
+-- WWI: Make borders less invasive so that terrain can be more easily read.
+
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 1.0 -- 5.0, -- thickness in pixels
+NDefines.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 1.0 -- 2.0, -- The center gradient is linear 1/255 per pixel for this many pixels
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 2.0 -- 25.0,
+NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 1.0 -- 11.0,
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 400; -- 400
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 700; -- 350
