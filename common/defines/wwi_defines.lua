b9f229a448e4fefdc9cf641def6f07d1348c6af8
Tom Slade <thomasjamesslade@gmail.com>
2020-05-17 23:35:39 +0100

    - Added 1914 start date and adjusted technology research dates to match.

---
+++ common/defines/wwi_defines.lua
@@ -0,0 +1,4 @@
+
+NDefines.NGame.START_DATE = "1911.1.1.12"
+NDefines.NGame.END_DATE = "1923.1.1.1"
+NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 2.1589	-- Based on the distance between Paris and Istanbul on the game map (~1045px).
