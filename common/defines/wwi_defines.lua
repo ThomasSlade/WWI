7c9e493ca46eded529a6612ba8fd6e0953005ea9
Tom Slade <thomasjamesslade@gmail.com>
2020-05-24 14:37:43 +0100

    - Initial balance pass for western-front combat. - Added placeholder land-battle doctrines.

--- common/defines/wwi_defines.lua
+++ common/defines/wwi_defines.lua
@@ -2,3 +2,5 @@
 NDefines.NGame.START_DATE = "1911.1.1.12"
 NDefines.NGame.END_DATE = "1923.1.1.1"
 NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 2.1589	-- Based on the distance between Paris and Istanbul on the game map (~1045px).
+NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0.1
+NDefines.NMilitary.LAND_COMBAT_ORG_DAMAGE_MODIFIER = 0.1
