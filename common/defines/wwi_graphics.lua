3b69baf11d7eb5b10a6f35ab1368d910dd657f91
TomJamesSlade <thomasjamesslade@gmail.com>
2022-06-19 20:25:18 +0100

    - Prototyped new terrain on Western Europe. West Germany, North France, Belgium, and Luxembourg now complete enough to battle in. - Removed Channel Islands from France and gave them to UK. - Changed country colours for a darker look. - Adjusted victory-point zoom thresholds.

--- common/defines/wwi_graphics.lua
+++ common/defines/wwi_graphics.lua
@@ -1,10 +1,21 @@
 -- WWI: Make borders less invasive so that terrain can be more easily read.
 
-NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 1.0 -- 5.0, -- thickness in pixels
-NDefines.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 1.0 -- 2.0, -- The center gradient is linear 1/255 per pixel for this many pixels
-NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 2.0 -- 25.0,
-NDefines.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 1.0 -- 11.0,
-NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 400; -- 400
-NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 700; -- 350
-NDefines.NGraphics.RIVER_FADE_FROM = 20.0; -- the last river endings got faded out, X distance from the ending...
-NDefines.NGraphics.RIVER_FADE_TO = 3.0;
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 1.0 -- 5.0, -- thickness in pixels
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 1.0 -- 2.0, -- The center gradient is linear 1/255 per pixel for this many pixels
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 2.0 -- 25.0,
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 1.0 -- 11.0,
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 400 -- 400
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 700 -- 350
+NDefines_Graphics.NGraphics.RIVER_FADE_FROM = 20.0 -- the last river endings got faded out, X distance from the ending...
+NDefines_Graphics.NGraphics.RIVER_FADE_TO = 3.0
+
+-- New Victory Point levels
+NDefines_Graphics.NGraphics.VICTORY_POINT_LEVELS = 2
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 10}
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {225, 600, 1200}
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {450, 700, 2000}
+
+-- Other tweaks
+NDefines_Graphics.NGraphics.RAILWAY_CAMERA_CUTOFF = 300.0
+
+NDefines_Graphics.NMapMode.MAP_MODE_MANPOWER_RANGE_MAX = 5000000
