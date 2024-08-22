39c8e5647c7efa6cfc6b1c87a21e6a076a666f49
TomJamesSlade <thomasjamesslade@gmail.com>
2024-08-22 11:24:18 +0100

    - Began adding custom government and law system to replace vanilla ideologies. Added Cabinet law and Government law. - Added placeholders for rationing, blockade policy, workers rights. - Added bookmark text for Russia. - Experimented with having War Support serve as War Exhaustion, which now starts at 100% and gradually ticks down. - Began trying to set up peace conferences so that only 25% of country can be annexed. - Fixed bug where '2nd Balkan War Ends' event (event_WWI_balkan_league_second_balkan_war.6) fires when only one of the anti-Bulgaria coalition is defeated. It should not only fire when all coalition enemies are defeated.

--- common/defines/wwi_defines.lua
+++ common/defines/wwi_defines.lua
@@ -2,12 +2,24 @@
 NDefines.NGame.START_DATE = "1911.1.1.12"
 NDefines.NGame.END_DATE = "1923.1.1.1"
 NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 2.1589	-- Based on the distance between Paris and Istanbul on the game map (~1045px).
+
+NDefines.NDiplomacy.PEACE_SCORE_SCALE_FACTOR = 0.25	-- Losers' total value times this factor becomes the default total peace conference score that is distributed to the winners.
+
+
+NDefines.NCountry.BASE_SURRENDER_LIMIT = 0.15 -- Base level of occupation required for country surrender. Effectively double this when warscore is 100%.
+NDefines.NCountry.DEFAULT_WAR_SUPPORT = 1.0	-- Default war support if not scripted otherwise.
+NDefines.NCountry.WAR_SUPPORT_OFFNSIVE_WAR = 0.0	-- Impact of being in offensive war
+NDefines.NCountry.WAR_SUPPORT_DEFENSIVE_WAR = 0.0	-- Impact of being in defensive war
+NDefines.NCountry.WAR_SUPPORT_TENSION_IMPACT = 0.0	-- Total impact of world tension
+
 NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0.053
 NDefines.NMilitary.LAND_COMBAT_ORG_DAMAGE_MODIFIER = 0.07
 NDefines.NMilitary.UNIT_DIGIN_CAP = 2
+
 NDefines.NProduction.BASE_FACTORY_SPEED = 5
 NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 2
 NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 2.5
+
 NDefines.NSupply.INFRA_TO_SUPPLY = 0.5
 NDefines.NSupply.NODE_STARTING_PENALTY_PER_PROVINCE = 0.4
 NDefines.NSupply.NODE_ADDED_PENALTY_PER_PROVINCE = 0.55
