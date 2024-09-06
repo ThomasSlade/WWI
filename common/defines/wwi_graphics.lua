ce932ccad26d80bd167b1ba935725e8798c521bb
TomJamesSlade <thomasjamesslade@gmail.com>
2024-09-06 19:00:45 +0100

    RUSSIA - Added basis of Russian focus tree, including attempt to liberalise, Zemgor, February Revolution, October Revolution, and some infrastructure/building. - Changing any law as Russia requires Tsar Favor. - Cannot remove Absolute Monarchy as Russia with Autocratic Tsardom.
    - Increased map icon draw distances a bit.
    - Fixed Danube Adriatic Railway decision referencing an invalid focus name.
    - Removed spam relating to modifier_SER_balkan_arbitration being checked outwith a peace conference, by gating that check behind is_in_peace_conference.
    - Fixed communist cabinet invalid modifier: now modifiers civ_to_mil and mil_to_civ conversion instead of the former twice.
    - Uncommented check for Albania already existing and fighting for independence in AUS_preempt_albanian_independence focus, as it was likely doing something.
    - Added resources to Petrograd.
    - Fixed typo in event_FRA_morocco_italy)agreement.1 (Prinetti-Barrere accord).
    - Removed debug decision toggle_weak. Replaced with custom command.
    - Added unlock unit files to /history/units to hopefully prevent crashes when someone researches a tech that unlocks a unit template.

--- common/defines/wwi_graphics.lua
+++ common/defines/wwi_graphics.lua
@@ -10,14 +10,14 @@ NDefines_Graphics.NGraphics.RIVER_FADE_FROM = 20.0 -- the last river endings got
 NDefines_Graphics.NGraphics.RIVER_FADE_TO = 3.0
 
 -- New Victory Point levels
+NDefines_Graphics.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 1000.0	-- Remove province names beyond this distance
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 15, 25}
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_CAPITAL_CUTOFF_MAX = 1500.0	--Capitals are special snowflakes, they need their own number
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {225, 600, 1200}
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF_MIN = 200.0 -- Min range for victory point text
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF_MAX = 1100.0 -- Max range for victory point text
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF_MAX = 1400.0 -- Max range for victory point text
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_DOT_CUTOFF_MIN = 300.0 -- Min range for victory point dot
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_DOT_CUTOFF_MAX = 1300.0 -- Max range for victory point text
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_MAX_VICTORY_POINTS_FOR_PERCENT = 1500.0 -- Default max value for point on the above range. It doesn't matter much if the VP value exceeds this, it'll be treated as max.
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_DOT_CUTOFF_MAX = 1600.0 -- Max range for victory point text
 
 -- Other tweaks
 NDefines_Graphics.NGraphics.RAILWAY_CAMERA_CUTOFF = 300.0
