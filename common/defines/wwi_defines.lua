f8d4673f8510690ad07cfe26dac8f16b70c4cbef
TomJamesSlade <thomasjamesslade@gmail.com>
2025-01-22 21:18:09 +0000

    BATTLE - More fiddling with values to get trench warfare trenchy - Adjusted vanilla values for dig in factor, dig in cap, dig in speed - Increased max dig-ins on 'digging_in', reduced bonus dig in speed a bit - Tried nerfing HMG org to make them less insane - Added new tiers for Field Arty, Heavy Arty, Siege Arty, HMGs, and Inf Equipment using first pass balance values
    AI
    - Reduced reluctance to accept call to arms due to dangerous front, as Bulgaria was still refusing the 2nd balkan war sometimes (may want to revert this if I ever reduce the Bulgarian front's tiles, or add an impassable border section, or do something else to reduce how many troops they need)
    - Changed all ideologies to have 'neutral' AI type, to try and stop AI being reluctant to accept diplomatic offers due to being democratic under the hood. Couldn't find the value driving these ideology-specific AI reluctances. Hopefully can change this back at some point if I ever understand it better.
    
    SERBIA
    - Made 'SER_austrian_jurisdiction' more dangerous but last less time
    - Accepting ultimatum removes claims on all three Bosnian states now
    - Royal Visit To Paris event now fires for France and Austria
    
    BALKAN LEAGUE
    - Tried reducing reluctance of Balkan War Instigator neighbors from dogpiling Bulgaria, because Turkey wasn't doing it much. But this is likely because Turkey keeps losing its entire army.
    
    MAP
    - Made some places bigger states to accommodate more factories
    
    GENERIC
    - Made tech indices start at 1 rather than 0
    - Adjusted conscription law rates to better suit what current countries need
    - Made Italo Turkish war start a few months later if it's AI controlled (historic, and assuming the Italian AI wants to make some preparations)
    - Fixed unexpected token 'abort' in FRA_morocco.txt caused by using add_ai_strategy incorrectly. This strategy is now enabled elsewhere, and should stop Italy invading the Balkans in Italo-Turkish war I hope.
    - Increased building slots once again because Serbia was struggling
    - Fixed yet more 'defence vs defense' errors in general traits
    - Rewordd some events: wwi_austria.3, event_wwi_balkan_league_second_balkan_war.2, event_TUR_albanian_independence.4, wwi_serbia.1

--- common/defines/wwi_defines.lua
+++ common/defines/wwi_defines.lua
@@ -4,6 +4,7 @@ NDefines.NGame.END_DATE = "1923.1.1.1"
 NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 2.1589	-- Based on the distance between Paris and Istanbul on the game map (~1045px).
 
 NDefines.NDiplomacy.PEACE_SCORE_SCALE_FACTOR = 0.03	-- Losers' total value times this factor becomes the default total peace conference score that is distributed to the winners.
+NDefines.NDiplomacy.FRONT_IS_DANGEROUS = -50 -- AI should be unwilling to enter accept a call to war if front is too dangerous.
 
 NDefines.NCountry.BASE_SURRENDER_LIMIT = 0.15 -- Base level of occupation required for country surrender. Effectively double this when warscore is 100%.
 NDefines.NCountry.DEFAULT_WAR_SUPPORT = 1.0	-- Default war support if not scripted otherwise.
@@ -13,7 +14,9 @@ NDefines.NCountry.WAR_SUPPORT_TENSION_IMPACT = 0.0	-- Total impact of world tens
 
 NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0.053
 NDefines.NMilitary.LAND_COMBAT_ORG_DAMAGE_MODIFIER = 0.07
-NDefines.NMilitary.UNIT_DIGIN_CAP = 2
+NDefines.NMilitary.DIG_IN_FACTOR = 0.02
+NDefines.NMilitary.UNIT_DIGIN_CAP = 5
+NDefines.NMilitary.UNIT_DIGIN_SPEED = 0.5
 
 NDefines.NProduction.BASE_FACTORY_SPEED = 5
 NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 2
