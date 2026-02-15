64f57e4baa54af4da1388b208dde922e10e5d4a9
ThomasSlade <thomasjamesslade@gmail.com>
2026-02-15 22:12:36 +0000

    Adding Food and Grain mechanics
    FOOD
    - Replaced Fuel stockpile with Food stockpile. When food falls below half, you begin losing stability.
    - Added grain to the whole map.
    - Manpower laws decrease food gain.
    - Reduced min_export from rationing laws.
    
    GENERAL
    - Offmap states now have increased infrastructure/railway time and increased local buildings slots.
    - Moved country names and food loc into /replacements/ directory. This has finally fixed Baden being named Baden-Wurtemburg.
    
    STILL TO DO
    - Added 'starvation threshold' and accompanying event at 25% food stocks. Below this, the idea is there's a chance that a region will suffer a famine, giving localised effects.

--- common/defines/wwi_defines.lua
+++ common/defines/wwi_defines.lua
@@ -2,6 +2,7 @@
 NDefines.NGame.START_DATE = "1911.1.1.12"
 NDefines.NGame.END_DATE = "1923.1.1.1"
 NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 1.991	-- Based on the distance between Paris and Istanbul on the game map (~1132.81px) over the crow-flies distance from Paris to Istanbul (2255.27km)
+NDefines.NGame.FUEL_RESOURCE = "tungsten"
 
 NDefines.NDiplomacy.PEACE_SCORE_SCALE_FACTOR = 0.03	-- Losers' total value times this factor becomes the default total peace conference score that is distributed to the winners.
 NDefines.NDiplomacy.FRONT_IS_DANGEROUS = -50 -- AI should be unwilling to enter accept a call to war if front is too dangerous.
@@ -12,6 +13,8 @@ NDefines.NCountry.WAR_SUPPORT_OFFNSIVE_WAR = 0.0	-- Impact of being in offensive
 NDefines.NCountry.WAR_SUPPORT_DEFENSIVE_WAR = 0.0	-- Impact of being in defensive war
 NDefines.NCountry.WAR_SUPPORT_TENSION_IMPACT = 0.0	-- Total impact of world tension
 NDefines.NCountry.MIN_FOCUSES_FOR_CONTINUOUS = 0	-- Focuses needed to unlock continuous focuses
+NDefines.NCountry.BASE_FUEL_GAIN_PER_OIL = 2		-- base amount of fuel gained hourly per excess oil
+NDefines.NCountry.BASE_FUEL_CAPACITY = 250000		-- base amount of fuel capacity
 
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 10 	--Base cost to unlock a regiment slot,
 NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0.053
@@ -38,10 +41,14 @@ NDefines.NAI.DESIRE_USE_XP_TO_UPDATE_LAND_TEMPLATE = 1.0	-- How quickly is desir
 NDefines.NAI.DIVISION_SUPPLY_RATIO_TO_MOTORIZE = 0.00	-- If supply ratio is less than this, consider motorizing any applicable nearby supply hub
 -- Changed so that AI more urgently pursues entrenchment doctrines.
 NDefines.NAI.VARIANT_CREATION_XP_RESERVE_LAND = 20	-- If the AI lacks army XP to create a variant it will reserve this much XP for variant creation so that it will eventually be able to create a variant.
+NDefines.NAI.MIN_WANTED_MAX_FUEL = 150	-- minimum value for wanted fuel buffers for AI (in thousands)
+NDefines.NAI.MAX_FACTORY_TO_TRADE_FOR_FUEL = 1
 
 NDefines.NProduction.BASE_FACTORY_SPEED = 5
 NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 2.5
 NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 2.5
+NDefines.NProduction.ANNEX_FUEL_RATIO = 0.2	-- How much fuel will be transferred on annexation
+NDefines.NProduction.CAPITULATE_FUEL_RATIO = 0.2	-- How much fuel will be transferred on capitulation
 
 NDefines.NBuildings.AIRBASE_CAPACITY_MULT = 25	-- Each level of airbase building multiplied by this, gives capacity (max operational value). Value is int. 1 for each airplane.
 
